8323b5b0fc50b9e3c24bb155c69cf0bf99c89fe2
Mihai Tarce <mihai.tarce@gmail.com>
2025-10-02 21:48:31 +0800

    Added author, view and like count to snippets.

--- src/lib/SnippetViewer.tsx
+++ src/lib/SnippetViewer.tsx
@@ -63,7 +63,7 @@ export function SnippetViewer({snippet, edit, close}) {
                 <button className={clsx({
                     "btn btn-square": true,
                     "btn-ghost border-0 text-base-content/50 hover:text-base-content": !snippet.draft_created,
-                    "btn-primary": snippet.draft_created
+                    "btn-soft btn-primary": snippet.draft_created
                 })}
                         aria-label="Edit"
                         onClick={() => edit(snippet)}>
@@ -85,10 +85,52 @@ export function SnippetViewer({snippet, edit, close}) {
             </div>
         </div>
 
-        <div className="mb-1 text-base-content/50">
-            {new Date(snippet.modified).toDateString()}
-            &nbsp;
-            {new Date(snippet.modified).toLocaleTimeString(undefined, {hour: "2-digit", minute: "2-digit"})}
+        <div className="flex flex-wrap items-center justify-between gap-1 mb-1">
+            <div className="flex-1 text-base-content/50">
+                {new Date(snippet.modified).toLocaleDateString(undefined, {
+                    weekday: "long",
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                })}
+            </div>
+
+            <div className="tooltip tooltip-bottom">
+                <div className="tooltip-content">
+                    user1<br/>
+                    user2<br/>
+                    ...
+                </div>
+                <button className="btn btn-sm btn-ghost pointer-events-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                    </svg>
+                    2
+                </button>
+            </div>
+
+            <button className="btn btn-sm btn-ghost pointer-events-none">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+                24
+            </button>
+
+            <div className="tooltip tooltip-bottom">
+                <div className="tooltip-content">
+                    user1<br/>
+                    user2<br/>
+                    ...
+                </div>
+
+                <button className="btn btn-sm btn-soft btn-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
+                    </svg>
+                    12
+                </button>
+            </div>
         </div>
 
         {snippet.revision__version &&
